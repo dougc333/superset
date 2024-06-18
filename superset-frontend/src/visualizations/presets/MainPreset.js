73ab406bf1354cf6b64b9c271dc769f6fad3b4d0
doug chang <dougc@stanfordalumni.org>
2024-06-18 14:08:04 -0700

    brokwn

--- superset-frontend/src/visualizations/presets/MainPreset.js
+++ superset-frontend/src/visualizations/presets/MainPreset.js
@@ -80,7 +80,7 @@ import { PivotTableChartPlugin as PivotTableChartPluginV2 } from '@superset-ui/p
 import { HandlebarsChartPlugin } from '@superset-ui/plugin-chart-handlebars';
 import { FilterPlugins } from 'src/constants';
 import { SupersetPluginChartHellp } from 'superset-plugin-chart-hellp';
-import { SupersetPluginHelloWorld } from 'superset-plugin-hello-world';
+//import { SupersetPluginHelloWorld } from 'superset-plugin-hello-world';
 import TimeTableChartPlugin from '../TimeTable';
 
 export default class MainPreset extends Preset {
